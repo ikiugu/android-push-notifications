f40b5bf20efa6a4427113e4b9d9802cd0b17cc8c
Alfred Ikiugu <ikiugualf@gmail.com>
2020-10-20 10:33:52 +0300

    added the navigation drawer activity

--- app/src/main/java/com/ikiugu/pushnotifications/SplashScreenActivity.java
+++ app/src/main/java/com/ikiugu/pushnotifications/SplashScreenActivity.java
@@ -1,14 +1,26 @@
 package com.ikiugu.pushnotifications;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    public static final long SPLASH_SCREEN_DELAY_TIME = 1500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_SCREEN_DELAY_TIME);
     }
 }
