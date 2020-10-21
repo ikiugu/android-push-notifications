28f1be90b1f131cd0187be4bb9f6a2f1250cb6ef
Alfred Ikiugu <ikiugualf@gmail.com>
2020-10-21 22:10:22 +0300

    cleaned up the settings activity screen

--- app/src/main/java/com/ikiugu/pushnotifications/SettingsActivity.java
+++ app/src/main/java/com/ikiugu/pushnotifications/SettingsActivity.java
@@ -37,12 +37,10 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 }
