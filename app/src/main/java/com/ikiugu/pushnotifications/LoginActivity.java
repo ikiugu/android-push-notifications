5d21e40b1b8e9c6a6bf035d359dea6be836ac765
Alfred Ikiugu <ikiugualf@gmail.com>
2020-10-23 11:29:29 +0300

    added back notifications

--- app/src/main/java/com/ikiugu/pushnotifications/LoginActivity.java
+++ app/src/main/java/com/ikiugu/pushnotifications/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.ikiugu.pushnotifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
@@ -49,6 +52,8 @@ public class LoginActivity extends BaseActivity {
         btnLogin = findViewById(R.id.btnLogin);
         client = getClient();
         coordinatorLayout = findViewById(R.id.coordinator);
+
+        createNotificationChannel();
 
 
         btnSendPush.setOnClickListener(new View.OnClickListener() {
@@ -246,5 +251,21 @@ public class LoginActivity extends BaseActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return false;
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
